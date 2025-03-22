ce29227564babc7142e53311261b7913dfba9613
mariatesler-sudo <mariatesler53@gmail.com>
2025-03-22 22:36:47 +0300

    vshbonelove

--- src/main/java/ru/tbank/fdsspring/controller/UserController.java
+++ src/main/java/ru/tbank/fdsspring/controller/UserController.java
@@ -1,15 +1,30 @@
 package ru.tbank.fdsspring.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/currencies")
 public class UserController {
-
-    @GetMapping("/user")
-    public ResponseEntity<String> getUsers(Long id) {
-        return ResponseEntity.ok("Ivan");
+    @GetMapping
+    public ResponseEntity<?> getCurrencies() {
+        return ResponseEntity.ok().build();
     }
 
+    @PostMapping
+    public ResponseEntity<?> postCurrencies() {
+        return ResponseEntity.ok().build();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCurrenciesById(@PathVariable String id) {
+        return ResponseEntity.ok().build();
+    }
+    @PostMapping("/{id}")
+    public ResponseEntity<?> postCurrenciesById(@PathVariable String id) {
+        return ResponseEntity.ok().build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCurrenciesById(@PathVariable String id) {
+        return ResponseEntity.ok().build();
+    }
 }
